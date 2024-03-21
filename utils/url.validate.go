e40d798135fe1c2211769dfda04d0d07d648004e
Seman <muhammad.seman030801@gmail.com>
2024-03-21 16:53:18 +0800

    renew url validate

--- utils/url.validate.go
+++ utils/url.validate.go
@@ -8,7 +8,7 @@ func IsValidURL(url string) bool {
 	// Regular expression pattern for URL validation
 	// This pattern checks for a URL that starts with http:// or https:// followed by a valid domain name
 	// and optional path and query parameters
-	pattern := `^(https?://)([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(/[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)?$`
+	pattern := `^(?:(?:https?|ftp|file):\/\/)?(?:www\.)?([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(/[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)?$`
 	match, _ := regexp.MatchString(pattern, url)
 	return match
 }
